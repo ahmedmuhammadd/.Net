6fd7ffc75729a9ac5be46e0f87e139e8e38062f1
ahmedmuhammadd <72719331+ahmedmuhammadd@users.noreply.github.com>
2025-07-08 17:31:59 +0300

    Add files via upload

---
+++ C44-G03-CS05.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            assignment();
+            Console.ReadKey();
+
+        }
+        public static void assignment()
+        {
+            //        1 - .Write a program that prints an identity matrix using for loop, in other
+            //words takes a value n from the user and shows the identity table of size n *n.
+
+            Console.Write("Enter the size of the identity matrix (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Identity Matrix of size {0}x{0}:", n);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                        Console.Write("1 ");
+                    else
+                        Console.Write("0 ");
+                }
+                Console.WriteLine();
+            }
+
+
+            //2 - Write a program in C# Sharp to find the sum of all elements of the array.
+
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            Console.WriteLine($"The sum of all elements of the array is: {sum}");
+
+
+            //3- Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.
+
+            int[] arr1 = { 1, 2, 3, 4, 5 };
+            int[] arr2 = { 6, 7, 8, 9, 10 };
+            int[] mergedArray = new int[arr1.Length + arr2.Length];
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                mergedArray[i] += arr1[i];
+            }
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                mergedArray[i + arr1.Length] += arr2[i];
+            }
+            foreach (var item in mergedArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            //4 - Write a program in C# Sharp to find maximum and minimum element in an
+            //array = { 2, 4, 2, 6, 6, 8, 9, 10, 30, 59, 58 };
+
+            int[] arr3 = { 2, 4, 2, 6, 6, 8, 9, 10, 30, 59, 58 };
+            Console.WriteLine("Max and Min of arr3 is " + arr3.Max().ToString() + "  ,  " + arr3.Min().ToString());
+
+
+
+            //5 - Write a program in C# Sharp to find the second largest element in an array.
+
+            Array.Sort(arr3);
+            Console.WriteLine("Second largest of arr3 is " + arr3[-2].ToString());
+
+
+
+
+
+
+            //7 - Given a list of space separated words, reverse the order of the words.
+
+            string sentence = "hello there i'am here";
+            string[] words = sentence.Split(' ');
+            Array.Reverse(words);
+            string reservedSentence = string.Join(" ", words);
+
+            //8- Write a program to create two multidimensional arrays of same size. Accept
+            //value from user and store them in first array. Now copy all the elements of
+            //first array on second array and print second array.
+
+            int arrSize = int.Parse(Console.ReadLine());
+            int[,] multiArr1 = new int[arrSize, arrSize];
+            int[,] multiArr2 = new int[arrSize, arrSize];
+
+            for (int i = 0; i < arrSize; i++)
+            {
+                Console.WriteLine("row 1");
+                for (int j = 0; j < arrSize;)
+                {
+                    Console.WriteLine("Enter a vlaues for row 1");
+                    bool result = int.TryParse(Console.ReadLine(), out multiArr1[i, j]);
+                    if (result)
+                        j++;
+                    else
+                        Console.WriteLine("Invalid input");
+                }
+            }
+
+            Array.Copy(multiArr1, multiArr2, multiArr1.Length);
+
+
+            //9 - Write a Program to Print One Dimensional Array in Reverse Order
+            Array.Reverse(multiArr2);
+            foreach (var item in multiArr2)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+        public static void session()
+        {
+            int[] numbers;
+            numbers = new int[3];
+            numbers[0] = 10;
+            numbers[1] = 20;
+            numbers[2] = 30;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine($"legth of numbers is {numbers.Length}, rank of arr is {numbers.Rank}");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"element {i} is {numbers[i]}");
+            }
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            int[] numbers01 = new int[3];
+            int[] numbers02 = new int[3] { 1, 2, 3 };
+            int[] numbers03 = new int[] { 1, 2, 3 };
+            int[] numbers04 = { 1, 2, 3 };
+
+
+            // 2d array
+
+            Console.WriteLine("2d arr");
+
+            int[,] marks = new int[2, 5];
+            //{
+            //    {1,2,3,4,5},
+            //    {6,7,8,9,10}
+            //};
+
+            marks[0, 0] = 10;
+            marks[0, 1] = 20;
+            marks[0, 2] = 30;
+            marks[0, 3] = 40;
+            marks[0, 4] = 50;
+            marks[1, 0] = 60;
+            marks[1, 1] = 70;
+            marks[1, 2] = 80;
+            marks[1, 3] = 90;
+            marks[1, 4] = 100;
+            Console.WriteLine(marks[1, 4]);
+            Console.WriteLine($"legth of numbers is {marks.Length}, rank of arr is {marks.Rank}");
+
+
+            for (int i = 0; i < marks.GetLength(0); i++)
+            {
+                Console.WriteLine($"enter the grade of student {i + 1}");
+                for (int j = 0; j < marks.GetLength(1);)
+                {
+                    Console.WriteLine($"enter the subjectnumber{j + 1}");
+
+                    bool isParsed = int.TryParse(Console.ReadLine(), out marks[i, j]);
+                    if (!isParsed)
+                        Console.WriteLine("invalid input");
+                    else
+                        j++;
+                }
+            }
+
+
+            for (int i = 0; i < marks.GetLength(0); i++)
+            {
+                for (int j = 0; i < marks.GetLength(1); j++)
+                {
+                    Console.WriteLine(marks[i, j]);
+                }
+
+            }
+
+
+            ////////////////
+            int[] arrMethods = { 1, 2, 3, 4, 5 };
+
+            //arrMethods.Reverse();
+            //Array.Clear(arrMethods);
+            //Array.Clear(arrMethods,1,5);
+            //Array.Resize(ref arrMethods, 20);
+
+
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assigment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1 - Write a program that allows the user to enter a number then print it.
+            Console.WriteLine("enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num);
+
+
+            //2 - Write C# program that converts a string to an integer, but the string contains non-numeric characters. And mention what will happen
+            Console.WriteLine("enter a string");
+            string str = Console.ReadLine();
+            int intStr = Convert.ToInt32(str);
+            Console.WriteLine(intStr);  //=> will not print 
+
+
+            //3-	Write C# program that Perform a simple arithmetic operation with floating-point numbers And mention what will happen
+            Console.WriteLine("enter a number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter a number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+
+
+            //4-	Write C# program that Extract a substring from a given string.
+            Console.WriteLine("enter a string");
+            string str1 = Console.ReadLine();
+            Console.WriteLine(str1.Substring(2, 3));
+
+
+            //5 - Write C# program that Assigning one value type variable to another and modifying
+            //the value of one variable and mention what will happen
+            int a = 1;
+            int b = a;
+            b = 2;
+            //b will be 2
+            //a will be 1
+            Console.WriteLine(a + " " + b);
+
+
+
+            //6-Write C# program that Assigning one reference type variable to another and modifying
+            //the object through one variable and mention what will happen
+            string c = "hello";
+            string d = c;
+            d = "hi";
+            //d will be "hi"
+            //c will be "hello"
+            Console.WriteLine(c + " " + d);
+
+
+
+            //7 - Write C# program that take two string variables and print them as one variable 
+            string val1 = "hello";
+            string val2 = "hello";
+            string val3 = val1 + val2;
+            Console.WriteLine(val3);
+
+
+
+            //8- Write a program that calculates the simple interest given the principal amount, rate of interest,
+            //and time. The formula for simple interest is Interest = (principal * rate * time) / 100.
+
+            Console.WriteLine("enter  principal number");
+            double principal = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter rate number");
+            double rate = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter time number");
+            double time = Convert.ToDouble(Console.ReadLine());
+            double interest = (principal * rate * time) / 100;
+            Console.WriteLine(interest);
+
+
+
+            //9-	Write a program that calculates the Body Mass Index (BMI) given a person's
+            //weight in kilograms and height in meters. The formula for BMI is  BMI = (Weight) / (Height * Height)
+
+            Console.WriteLine("enter weight number");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter height number");
+            double height = Convert.ToDouble(Console.ReadLine());
+            double BMI = weight / (height * height);
+            Console.WriteLine(BMI);
+
+
+
+            //10-	Write a program that uses the ternary operator to check if the temperature is too hottoo cold,
+            //or just good. Assign the result in a variable then display the result. Assume that below 
+            //10 degrees is "Just Cold", above 30 degrees is "Just Hot", and anything else is "Just Good".
+
+            Console.WriteLine("enter temperature number");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            string result = temp < 10 ? "Just Cold" : temp > 30 ? "Just Hot" : "Just Good";
+            Console.WriteLine(result);
+
+
+
+            //11- Write a program that takes the date from the user and displays it in various formats using string interpolation.
+            //Ex:
+            //Today’s date : 20 , 11 , 2001
+            // Today's date : 20 / 11 / 2001
+            //Today's date : 20 – 11 – 2001
+            // Get input from user
+            Console.Write("Enter day: ");
+            int day = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter month: ");
+            int month = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Today's date : {day} , {month} , {year}");
+            Console.WriteLine($"Today's date : {day} / {month} / {year}");
+            Console.WriteLine($"Today's date : {day} – {month} – {year}");
+
+
+            //12-	 What is the output of the following C# code?
+
+            //  DateTime date = new DateTime(2024, 6, 14); Console.WriteLine($"The event is on {date:MM/dd/yyyy}");
+            //The event is on 06/14/2024
+
+
+
+
+
+
+            //13 - Which of the following statements is correct about the C#.NET code snippet given below?
+
+            /*
+              int d;
+             d = Convert.ToInt32(!(30 < 20));
+             */
+
+            //      ==========>      f)	A value 1 will be assigned to d.
+
+
+
+
+
+
+
+            //14 - Which of the following is the correct output for the C# code given below?
+
+            //Console.WriteLine(13 / 2 + " " + 13 % 2);
+
+            //  ==========>          a) 6.5 1
+
+
+
+
+
+
+            //15 - What will be the output of the C# code given below?
+
+            //int num = 1, z = 5;
+
+            //            if (!(num <= 0))
+            //                Console.WriteLine(++num + z++ + " " + ++z);
+            //            else
+            //                Console.WriteLine(--num + z-- + " " + --z);
+            //=>>>>>>>>>> d) 7 7
+
+        }
+
+
+    }
+}
